7a731cb18caed60f5ae8421169760d31e1eca605
Anastasiia Fedorchuk <phoebebuffay1989@gmail.com>
2025-03-27 18:35:49 +0100

    First part of the game

---
+++ main.js
@@ -0,0 +1,32 @@
+// function getComputerChoice() {
+//   if (
+//     Math.floor(Math.random() * 11) >= 0 &&
+//     Math.floor(Math.random() * 11) <= 4
+//   ) {
+//     return "Rock";
+//   } else if (
+//     Math.floor(Math.random() * 11) >= 5 &&
+//     Math.floor(Math.random() * 11) <= 8
+//   ) {
+//     return "Paper";
+//   } else {
+//     return "Scissors";
+//   }
+// }
+// console.log(getComputerChoice());
+
+let name = function getHumanChoice() {
+  return prompt("Please, make your step.");
+};
+console.log(name());
+// function getComputerChoice() {
+//   if (Math.random() * 11 >= 0 && Math.random() * 11 <= 0.33) {
+//     return "Rock";
+//   } else if (Math.random() * 11 >= 0.34 && Math.random() * 11 <= 0.79) {
+//     return "Paper";
+//   } else {
+//     return "Scissors";
+//   }
+// }
+
+// console.log(getComputerChoice());
